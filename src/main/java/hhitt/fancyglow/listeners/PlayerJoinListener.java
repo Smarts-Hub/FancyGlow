6f98f2131a3ffa88c561ede7ede1a7a00eb958e6
OakLoaf <oak@beaconstudios.org>
2025-08-17 19:44:59 +0100

    Added permission check on join

---
+++ src/main/java/hhitt/fancyglow/listeners/PlayerJoinListener.java
@@ -0,0 +1,25 @@
+package hhitt.fancyglow.listeners;
+
+import hhitt.fancyglow.FancyGlow;
+import hhitt.fancyglow.managers.GlowManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerJoinListener implements Listener {
+
+    private final GlowManager glowManager;
+
+    public PlayerJoinListener(FancyGlow plugin) {
+        this.glowManager = plugin.getGlowManager();
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        if (!player.hasPermission("fancyglow.all_colors")) {
+            glowManager.removePlayerFromAllTeams(player);
+        }
+    }
+}
